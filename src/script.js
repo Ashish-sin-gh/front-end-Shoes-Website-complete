48c1796ada2b92e3c3dfa255516f844408c57ebd
Ashish Kumar Singh <ashish159singh@gmail.com>
2024-08-20 16:31:36 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,30 @@
+const humburger = document.getElementById("hamburger-img");
+const burgerDropDown = document.getElementById("burgerDropDown");
+
+const bigShoes = document.getElementById("bigShoes");
+const shoes1 = document.getElementById("shoes1");
+const shoes2 = document.getElementById("shoes2");
+const shoes3 = document.getElementById("shoes3");
+
+
+
+burgerDropDown.addEventListener("click", ()=>{
+    burgerDropDown.classList.toggle('hidden');
+});
+
+humburger.addEventListener("click", ()=>{
+    burgerDropDown.classList.toggle('hidden');
+});
+
+shoes1.addEventListener('click', ()=>{
+    bigShoes.src = "/assets/images/big-shoe1.png";
+});
+
+shoes2.addEventListener('click', ()=>{
+    bigShoes.src = "/assets/images/big-shoe2.png";
+});
+
+shoes3.addEventListener('click', ()=>{
+    bigShoes.src = "/assets/images/big-shoe3.png";
+    console.log("shoes 3 touched")
+}); 
